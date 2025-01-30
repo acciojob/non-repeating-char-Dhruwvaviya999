d8198cc1a664a0276c3d7bf7502578a6d3122b2f
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-01-30 06:19:59 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
+	// Write your code here
+	let obj = {};
+	let strArr = str.split("");
+	for(let char of strArr){
+		obj[char] = (obj[char] || 0) + 1;
+	};
+	
+	for(let char in obj){
+		if(obj[char] === 1){
+			return char;
+		};
+	};
+
+	return null;
+
+};
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
